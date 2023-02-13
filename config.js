222141edb88da7e16152ccfc786489218a1da6ff
anhgiang22112003 <118231311+anhgiang22112003@users.noreply.github.com>
2023-02-14 00:57:54 +0700

    Add files via upload
    create cad

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 14/2';
+const titleCard = 'Tặng vợ a ';
+const contentCard = 'Chúc honey của anh 14/2  tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = '319760624_701242068241315_7727970520026605894_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
